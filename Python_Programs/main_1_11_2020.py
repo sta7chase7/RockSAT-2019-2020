4b07c2732de669830c130a019e170400b590fd80
NathanClapp <45773714+NathanClapp@users.noreply.github.com>
2020-01-21 01:54:10 -0700

    -Removed UV sensor aspects

--- Python_Programs/main_1_11_2020.py
+++ Python_Programs/main_1_11_2020.py
@@ -15,7 +15,6 @@ from adafruit_motorkit import MotorKit
 import Sensors.Accelerometer_Code_ADXL377
 import Sensors.Distance_Code_vl53l0x
 import Sensors.Temperature_Code_TMP36
-import Sensors.UV_Code_veml6070
 
 #
 
@@ -28,7 +27,6 @@ import Sensors.UV_Code_veml6070
 #need to define input pins
 xyz_accel_list = accel_create_input_3ax(x_pin,y_pin,z_pin)
 temp_input = temp_create_input(temp_pin)
-uv_input = uv_create_input()
 distance_input = distance_create_input()
 
 #read and print data:
@@ -47,9 +45,4 @@ distance_input = distance_create_input()
 #
 #set delay for time between GSE-On and launch (see CDR)
 #offset timer by GSE-On delay
-
-#
-#upon launch,
-#
-#upon  
 #--------------------------------
