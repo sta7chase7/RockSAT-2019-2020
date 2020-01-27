a964dcbed589a72484c99ec8c44e839b7467cfb3
sta7chase7 <52681087+sta7chase7@users.noreply.github.com>
2020-01-26 17:28:01 -0700

    Initial Commit!

---
+++ Python_Programs/Camera/video_transfer.py
@@ -0,0 +1,11 @@
+#Author:              Mac Grove
+#
+#Language:            Python/ Bash
+#
+#General Description: Program for video retrival
+#
+#Version:			  1.0
+#
+#Copyright:			  CCoC RocketSat 2019/2020
+
+import os
