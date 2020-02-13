72202487a17d95cb8eec78c5718738f16072c24d
Nathan <gnathanq20@gmail.com>
2020-02-13 14:11:06 -0700

    updated motor code

--- Python_Programs/Arm_Control/Motor_Lib.py
+++ Python_Programs/Arm_Control/Motor_Lib.py
@@ -4,10 +4,10 @@ from adafruit_motor import stepper
 #________________________________
 #Inputs:
 #move_cmd - 0 or 1 - whether or not the motor is being commanded to move
-#direction - 0 or 1 - backward or forward, respectively
-#running - 0 or 1 - whether or not the motor is currently running
+#direction -  or 1 or 2 - null, backward or forward, respectively
+#running - 0 or 1 or 2 - null, not running, running, respectively
 #________________________________
-def full_move(move_cmd,direction,running):
+def full_move(move_cmd,direction=0,running=0):
     kit = MotorKit()
     direction_map = [BACKWARD, FORWARD]
     step_count = 100
@@ -15,19 +15,20 @@ def full_move(move_cmd,direction,running):
     if move_cmd == 1:
         #set move_cmd state to true
         status_array.append(0)
+        if running == 2:
+            continue
         if running == 1:
-            break
-        if running == 0:
             try:
                 #update direction state in output list
                 status_array.append(direction)
                 #show whether or not the process is running
-                status_array.append(1)
+                status_array.append(2)
+
                 for i in range(step_count):
                     kit.stepper1.onestep(direction=stepper.direction_map[direction], style=stepper.DOUBLE)
                     time.sleep(sleep_between_steps)
             except:
-                break
+                continue
     if move_cmd == 0:
         status_array = [0,0,0]
     return status_array
