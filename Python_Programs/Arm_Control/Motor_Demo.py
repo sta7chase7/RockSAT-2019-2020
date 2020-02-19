fde826e8599303940984c95b62abc6ef4fee6771
Nathan <gnathanq20@gmail.com>
2020-02-19 10:59:27 -0700

    test

--- Python_Programs/Arm_Control/Motor_Demo.py
+++ Python_Programs/Arm_Control/Motor_Demo.py
@@ -3,7 +3,9 @@ import time
 from adafruit_motorkit import MotorKit
 
 kit = MotorKit()
-
-for i in range(100):
-    kit.stepper1.onestep()
-    time.sleep(0.001)
+try:
+    for i in range(100):
+        kit.stepper1.onestep()
+        time.sleep(0.001)
+except:
+    print("error!")
