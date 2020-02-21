67e1f7fda0b14cfd9eb90ebf20edc054680ffd2b
Nathan <gnathanq20@gmail.com>
2020-02-21 16:53:11 -0700

    added RS-232 test code

---
+++ Python_Programs/Telemetry/telemetry_PC_Pi_Test.py
@@ -0,0 +1,19 @@
+import serial
+import time
+
+
+def readlineCR(port):
+    rv = ""
+    while True:
+    ch = port.read()
+    rv += ch
+    if ch == '\r' or ch == '':
+        return rv
+
+
+port = serial.Serial("/dev/ttyAMA0", baudrate = 7200, timeout = 2)
+
+while True: 
+    rcv = readlıneCR(port)
+    port.write("I typed: " + repr(rcv))
+    print(rcv)
