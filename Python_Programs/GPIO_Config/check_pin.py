8ae2f375fc68b164f664173dbe4c360030c9897a
Nathan <gnathanq20@gmail.com>
2020-03-11 12:23:52 -0600

    updates

--- Python_Programs/GPIO_Config/check_pin.py
+++ Python_Programs/GPIO_Config/check_pin.py
@@ -2,5 +2,17 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 
+#BCM correlates to an integer for gpio_function
+
+#0 = GPIO.OUT
+#1 = GPIO.IN
+#40 = GPIO.SERIAL
+#41 = GPIO.SPI
+#42 = GPIO.I2C
+#43 = GPIO.HARD_PWM
+#-1 = GPIO.UNKNOWN
+
+#The GPIO.UNKNOWN pins should be our primary candidates for TE-1 and TE-R inputs
+
 for g in range(54):
     print("gpio {} is {}".format(g, GPIO.gpio_function(g)))
--- Python_Programs/GPIO_Config/edge_detect.py
+++ Python_Programs/GPIO_Config/edge_detect.py
@@ -1,8 +1,17 @@
-import RPi.GPIO
+import RPi.GPIO as GPIO
 
-channel = 
+GPIO.setmode(GPIO.BCM)
 
-GPIO.add_event_detect(channel, GPIO.RISING)  # add rising edge detection on a channel
+in_channel = 31
+out_channel = 30
 
-if GPIO.event_detected(channel):
+GPIO.setup(in_channel, GPIO.IN)
+GPIO.setup(out_channel, GPIO.OUT)
+
+GPIO.add_event_detect(in_channel, GPIO.RISING)  # add rising edge detection on a channel
+
+GPIO.output(out_channel, GPIO.HIGH)
+print("output pin set to high")
+
+if GPIO.event_detected(in_channel):
     print('output!')
