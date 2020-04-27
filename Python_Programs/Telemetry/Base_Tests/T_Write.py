97c2879fb1571b77b36248aeb919eddac50e4ce4
NathanClapp <45773714+NathanClapp@users.noreply.github.com>
2020-04-27 13:25:46 -0600

    this push includes working RS232 code

--- Python_Programs/Telemetry/Base_Tests/T_Write.py
+++ Python_Programs/Telemetry/Base_Tests/T_Write.py
@@ -3,7 +3,7 @@ import serial
 
 ser = serial.Serial(
         port='/dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
-        baudrate = 9600,
+        baudrate = 19200,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
@@ -18,3 +18,32 @@ while 1:
         time.sleep(1)
         counter += 1
         print(thing_to_send)
+
+array_collect():
+        #Define default array elements
+                #include non-standard escape character as last array element
+        #mission time [locally counted by pi's]
+                #ms?
+        #TE-(1,R) line state
+                #0 or 1 for each
+        #sensor data [ADXL377,TMP006,TMP36,Distance]
+                #make substructures for each
+                #decide upon & record units of measurement
+        #motor state info
+                #See relevant script; move_cmd, direction, running?
+        #MADV state info [power, recording state]
+                #Get further details once it's tested
+        #note to add static indexing to values
+        #Regularly convert all data to bytes
+        #work out timing; function should wait for a time interval, not the presence of all data elements, to 
+        #return array of byte values
+
+array_filesave():
+        #take input from array_collect
+        #convert to [numpy?] array and save as txt every few minutes
+        #figure out how to avoid having missed values
+
+telemetry_send():
+        #Take in byte array
+        #Send it with no delay
+        
