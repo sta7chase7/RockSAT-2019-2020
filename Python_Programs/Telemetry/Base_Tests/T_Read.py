a4040533e44ae85b7f72f30570367745d6339b56
NathanClapp <45773714+NathanClapp@users.noreply.github.com>
2020-04-27 10:57:00 -0600

    update telemetry code

---
+++ Python_Programs/Telemetry/Base_Tests/T_Read.py
@@ -0,0 +1,11 @@
+import time
+import serial
+
+ser = serial.Serial(
+        port='/dev/ttyUSB0',
+        baudrate = 9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+)
