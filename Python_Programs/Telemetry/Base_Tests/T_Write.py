d22a889ea0093a7c4f273cef5eb73c4ea4186252
NathanClapp <45773714+NathanClapp@users.noreply.github.com>
2020-04-27 11:35:41 -0600

    fiddling with binary

--- Python_Programs/Telemetry/Base_Tests/T_Write.py
+++ Python_Programs/Telemetry/Base_Tests/T_Write.py
@@ -3,7 +3,7 @@ import serial
 
 ser = serial.Serial(
         port='/dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
-        baudrate = 9600,
+        pytbaudrate = 9600,
         parity=serial.PARITY_NONE,
         stopbits=serial.STOPBITS_ONE,
         bytesize=serial.EIGHTBITS,
@@ -12,7 +12,8 @@ ser = serial.Serial(
 counter=0
 
 while 1:
-        ser.write(counter)
+        #encode the counter variable to a binary string
+        ser.write(format(counter, "b"))
         time.sleep(1)
         counter += 1
-        print(counter)
+        print(format(counter, "b"))
