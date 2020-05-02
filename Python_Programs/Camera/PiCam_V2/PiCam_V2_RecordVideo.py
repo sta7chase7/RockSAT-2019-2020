6807549bd65e2868d68bb1eaa586fd84f7aae8b0
PurpleAmaranth <36141949+PurpleAmaranth@users.noreply.github.com>
2020-05-01 18:52:14 -0600

    Update PiCam_V2_RecordVideo.py
    New Pi Camera Code Still needs a Main Init and Start-up Batch in Bash

--- Python_Programs/Camera/PiCam_V2/PiCam_V2_RecordVideo.py
+++ Python_Programs/Camera/PiCam_V2/PiCam_V2_RecordVideo.py
@@ -1,28 +1,59 @@
-import time
-from picamera import PiCamera
+import time # Controls Time Intervals and sleep
+import datetime #Allow recording of date and time stamps
+from datetime import datetime #Need to ensure datetime module is installed
+import os.path # Allows management of path names
+import picamera # Nifty Pi Camera Commands
+from picamera import PiCamera #Make sure function PiCamera is defined
 
-#Needs update work on state assignments/returns
+def PiCam_Record():
+    """ Controls the recording loop and saving intervals for the pi camera"""
 
-#possible mode states: "event" xor "time" xor "hybrid"
-def PiCam_Record(mode,save_dir_string,recording_interval_s=None,event_call=None):
-
-    #Move the line below to main_pi3b? - making a new object may cause delays.
+    #Names camera and specifies recording features:
     camera = PiCamera()
+    camera.framerate = 30
+    video_stabilization = True
 
-    camera.start_recording(save_dir_string)
-    camera_state = 0
+    #Start Recording and Store Video Files to the specified path
+    #Concatenate the video name, datetime, and file type
+    #Only store as .h624 format
+    save_path = "/home/pi/Desktop/PiVideo/"
+    #Stores as .h624 format (Pi Cam Default), can be converted easily with VLC Player
+    file_timestamp = datetime.now().strftime("%y%m%d_%H%M%S.h264") 
+    file_name = "Pi_Vid_" + file_timestamp
+    video_name = os.path.join(save_path, file_name)
 
-    #Have the camera record for a set interval
-    if mode == "time":
-        time.sleep(recording_interval_s)
+    try:
+        #Start recording for duration of time delay:
+        camera.start_recording(video_name)
+        #Test Print:
+        #print(video_name, "is recording and things!")
+
+        #(WARNING: Measured in seconds, NOT mili-seconds!!!)
+        time.sleep(60)
         camera.stop_recording()
 
-    #Event mode assumes that pi cam will be recording already.
-    if mode == "event":
-        if event_call == True:
-            camera.stop_recording()
+    #Always end picamera module commands with camera.close():
+    finally:
+        camera.close()
+        #Test Print:
+        #print("Camera is done flurbling!")
+    
+    return;
 
-    #Stop if either of the two events gets triggered
-    if mode == "hybrid":
-        if (bool(time.sleep(recording_interval_s)) == False) or (event_call == True):
-            camera.stop_recording()
+
+#For Main create a while loop:
+#While Pi Zero = On record
+#Function will record in set interval (seconds).
+#Note: other camera commands might be set in miliseconds):
+#Example:
+"""
+if__name__=="__main__"
+
+while:                                  #Sys read on, voltage reading, or other benchmark
+    PiCam_Record()                      #will record in intervals over loop
+
+    elif:
+        break
+        
+"""
+
