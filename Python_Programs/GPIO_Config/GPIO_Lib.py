d0f4692437e39ef38fb876b2ff07fe5804f96831
Nathan Clapp <gnathanq20@gmail.com>
2020-05-31 16:00:42 -0600

    added more gpio functionality

--- Python_Programs/GPIO_Config/GPIO_Lib.py
+++ Python_Programs/GPIO_Config/GPIO_Lib.py
@@ -4,20 +4,26 @@ import time
 #These functions' pins need to be tested with a voltmeter
 
 #Trigger voltage changes on a BCM channel (see pinout.xyz)
-def TE_1_Event(out_channel, high_time):
-    GPIO.output(out_channel, GPIO.HIGH)
+def TE_1_Event(in_channel, out_channel, high_time):
+    GPIO.add_event_detect(in_channel, GPIO.RISING)
+    if GPIO.event_detected(in_channel)
+        GPIO.output(out_channel, GPIO.HIGH)
     #time.sleep may cause the arm function to start extend/retract cycle WHILE signal is high, not just once around. Test!
     #suspend state for 2s
-    time.sleep(high_time)
+        time.sleep(high_time)
     #Reset pin state
-    GPIO.output(out_channel, GPIO.LOW)
+        GPIO.output(out_channel, GPIO.LOW)
+        GPIO.cleanup()
     #Note: remove all print statements before final payload integration
     return 1
 
-def TE_R_Event(out_channel, high_time):
-    GPIO.output(out_channel, GPIO.HIGH)
-    time.sleep(high_time)
-    GPIO.output(out_channel, GPIO.LOW)
+def TE_R_Event(in_channel, out_channel, high_time):
+    GPIO.add_event_detect(in_channel, GPIO.RISING)
+    if GPIO.event_detected(in_channel)
+        GPIO.output(out_channel, GPIO.HIGH)
+        time.sleep(high_time)
+        GPIO.output(out_channel, GPIO.LOW)
+    GPIO.cleanup()
     return 1
 
 #Get data on MADV terminal timing in order to fill these out.
@@ -27,35 +33,41 @@ def MADV_On(out_channel):
     GPIO.output(out_channel, GPIO.HIGH)
     time.sleep(2)
     GPIO.output(out_channel, GPIO.LOW)
+    GPIO.cleanup()
     return 1
 
 def MADV_Off(out_channel, high_time):
     GPIO.output(out_channel, GPIO.HIGH)
     time.sleep(high_time)
     GPIO.output(out_channel, GPIO.LOW)
+    GPIO.cleanup()
     return 1
 
 def MADV_Record_Start(out_channel, high_time):
     GPIO.output(out_channel, GPIO.HIGH)
     time.sleep(high_time)
     GPIO.output(out_channel, GPIO.LOW)
+    GPIO.cleanup()
     return 1
 
 def MADV_Record_Stop(out_channel, high_time):
     GPIO.output(out_channel, GPIO.HIGH)
     time.sleep(high_time)
     GPIO.output(out_channel, GPIO.LOW)
+    GPIO.cleanup()
     return 1
 
 def MADV_Recording_Mode(out_channel, high_time):
     GPIO.output(out_channel, GPIO.HIGH)
     time.sleep(high_time)
     GPIO.output(out_channel, GPIO.LOW)
+    GPIO.cleanup()
     return 1
 
 def MADV_Storage_Mode(out_channel, high_time):
     GPIO.output(out_channel, GPIO.HIGH)
     time.sleep(high_time)
     GPIO.output(out_channel, GPIO.LOW)
+    GPIO.cleanup()
     return 1
 
